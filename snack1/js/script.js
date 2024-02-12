2b8195b111bf8025d5c227d92d833cdaf1224734
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-12 17:04:22 +0100

    Salvo il testo

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,1 +1,11 @@
 //PROGRAMMA PAROLA INVERTITA
+
+const button = document.querySelector("#button");
+let userText;
+
+//salvo il testo inserito
+button.addEventListener("click", function(){
+
+    userText = document.querySelector("#text").value;
+    console.log(userText);
+})

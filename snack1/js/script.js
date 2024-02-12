e31ae62b712431b262338897238dac9c7996ecb3
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-12 17:16:31 +0100

    Primo Snack completo

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -8,4 +8,20 @@ button.addEventListener("click", function(){
 
     userText = document.querySelector("#text").value;
     console.log(userText);
+
+
+    //creo la function per invertire il testo
+    //Prima divido ogni lettera e la inserisco in un array
+    //inverto l'ordine dell'array
+    //alla fine riunisco gli elementi dell'array
+
+    function invertString(str) {
+        return str.split("").reverse().join("");
+    }
+
+    //eseguo la funzione per invertire il testo precedentemente salvato
+    const invertWord = invertString(userText);
+    console.log(invertWord);
 })
+
+  

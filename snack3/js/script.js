b25c5b1ce5c77738131ed72edb5ad6650c23da29
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-12 21:48:09 +0100

    creo function

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,5 +1,25 @@
 //PROGRAMMA 1 ARRAY 2 NUMERI
 
+//salvo l'array di numeri
+const arrayNumbers = [0, 1, 2, 3, 4, 5, 6];
 
+//creo la function per filtrarla
+function cutArray(array, posizioneMin, posizioneMax) {
+
+    //eseguo un controllo dei limiti
+    if (posizioneMin < 0 || posizioneMax > array.length) {
+        return "Valori di posizione non validi";
+    }
+
+    //controllo se il min è minore del max
+    if (posizioneMin > posizioneMax) {
+        return "Il numero deve essere minore del numero di posizione massima";
+    }
+
+    //creo un array vuoto
+    
+
+
+}
 
   
